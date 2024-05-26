9042291f9e2cf6a86873acaf17c0125c85013884
Alefer Reinert <aleferreinert@gmail.com>
2024-05-26 11:10:53 -0300

    Refatored card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,22 +12,24 @@ export function Card({ title, description, theme = 'default' }: CardProps) {
 	const themeStyles = theme === 'dark' ? themeDark : themeDefault
 
 	return (
-		<DialogTrigger
-			title='Visualizar'
-			className={`
-				${themeStyles}
-				rounded-md p-[1.125rem] relative outline-none
-				border-2 border-transparent text-left 
-				flex flex-col aspect-[2/1] sm:aspect-[7/5]
-				transition-all hover:border-slate-600 focus-visible:border-lime-400
-			`}
-		>
-			<h2 className='text-sm font-medium mb-3'>{title}</h2>
-			<p className='text-slate-400 text-sm leading-6 line-clamp-6'>{description}</p>
-
-			{title !== 'Adicionar nota' && (
-				<div className='absolute left-[-2px] right-[-2px] bottom-[-2px] top-1/2 rounded-b-md bg-gradient-to-t from-black/60 to-transparent to-50%'></div>
-			)}
+		<DialogTrigger asChild>
+			<button title='Visualizar' className='relative outline-none group'>
+				<div
+					className={`
+						${themeStyles} 
+						p-[1.125rem] rounded-md text-left border-2 border-transparent transition-all
+						flex flex-col aspect-[2/1] sm:aspect-[7/5]
+						group-hover:border-slate-600
+						group-focus-visible:border-lime-400
+					`}
+				>
+					<h2 className='text-sm font-medium mb-3'>{title}</h2>
+					<p className='text-slate-400 text-sm leading-6 line-clamp-6'>{description}</p>
+				</div>
+				{title !== 'Adicionar nota' && (
+					<div className='pointer-events-none absolute inset-0 top-1/2 group-hover:inset-[2px] group-focus-visible:inset-[2px]  rounded-b-md bg-gradient-to-t from-black/60 to-transparent to-50%'></div>
+				)}
+			</button>
 		</DialogTrigger>
 	)
 }
