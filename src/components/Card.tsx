2bd02bb1e773424fa88b29550742a42137d42ec4
Alefer Reinert <aleferreinert@gmail.com>
2024-05-26 10:05:49 -0300

    Refatored card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 export function Card({ title, description, theme = 'default' }: CardProps) {
 	const themeDefault = 'bg-slate-700 text-slate-200 '
-	const themeDark = 'bg-slate-800 text-slate-300 shadow-lg shadow-black/25 '
+	const themeDark = 'bg-slate-800 text-slate-300 shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] '
 	const themeStyles = theme === 'dark' ? themeDark : themeDefault
 
 	return (
@@ -16,17 +16,17 @@ export function Card({ title, description, theme = 'default' }: CardProps) {
 			title='Visualizar'
 			className={`
 				${themeStyles}
-				rounded-md p-5 overflow-hidden relative outline-none
-				ring-2 ring-transparent text-left 
-				flex flex-col min-h-28 sm:h-60
-				transition-all hover:ring-slate-600 focus-visible:ring-lime-400
+				rounded-md p-[1.125rem] relative outline-none
+				border-2 border-transparent text-left 
+				flex flex-col aspect-[2/1] sm:aspect-[7/5]
+				transition-all hover:border-slate-600 focus-visible:border-lime-400
 			`}
 		>
 			<h2 className='text-sm font-medium mb-3'>{title}</h2>
 			<p className='text-slate-400 text-sm leading-6 line-clamp-6'>{description}</p>
 
 			{title !== 'Adicionar nota' && (
-				<div className='absolute inset-0 top-1/2 bg-gradient-to-t from-black/60 to-transparent to-50%'></div>
+				<div className='absolute left-[-2px] right-[-2px] bottom-[-2px] top-1/2 rounded-b-md bg-gradient-to-t from-black/60 to-transparent to-50%'></div>
 			)}
 		</DialogTrigger>
 	)
